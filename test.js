47ed7f803e56d67299d4085a3694ace54dafe546
mengliner <1219948661@qq.com>
2023-05-18 16:08:53 +0800

    111

--- test.js
+++ test.js
@@ -1,1 +1,9 @@
-import react;
+/*
+ * @Author: mengliner 1219948661@qq.com
+ * @Date: 2023-05-18 14:34:38
+ * @LastEditors: mengliner 1219948661@qq.com
+ * @LastEditTime: 2023-05-18 16:04:12
+ * @FilePath: \first\test.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+  
